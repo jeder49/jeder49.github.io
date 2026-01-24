516e0abdb161641c6165ea23e1de3b3fa21550d1
Jelly <67157454+jeder49@users.noreply.github.com>
2026-01-24 18:24:15 +0100

    change workflow and nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,14 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
-  css: ['./app/assets/css/main.css']
+  css: ['./app/assets/css/main.css'],
+  
+  app: {
+    baseURL: '/jeder49.github.io/', // Replace with your repo name
+    buildAssetsDir: 'assets',
+  },
+  
+  nitro: {
+    preset: 'github_pages'
+  }
 })
