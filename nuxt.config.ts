cebab009dcae30360c82e647448a7619454e318e
Jelly <67157454+jeder49@users.noreply.github.com>
2026-01-24 18:56:28 +0100

    update css path (again)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
-  css: ['~/app/assets/css/main.css'],
+  css: ['~/assets/css/main.css'],
   
   app: {
     baseURL: '/',
